959bdc58ed21057ab9778e82cae66006e0dc2bd2
sonu kumar <gautamrocky909621@gmail.com>
2026-04-05 09:49:14 +0530

    Runtime built in exception converted in to the custom exception

---
+++ src/main/java/com/workouttrackerapi/common/exceptions/ExerciseAllRedyExist.java
@@ -0,0 +1,9 @@
+package com.workouttrackerapi.common.exceptions;
+
+public class ExerciseAllRedyExist extends RuntimeException {
+
+    public ExerciseAllRedyExist(String message) {
+        super(message);
+    }
+
+}
